b19ae3191f88a74405d533240f11b77acb568487
kcsamm <kcsamm11@gmail.com>
2022-09-06 01:10:37 +0100

    Further work on dashboard

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -1,11 +1,12 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
 
+
 const Dashboard = () => {
     return (
         <AppLayout
             header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight mt-4">
                     Dashboard
                 </h2>
             }>
