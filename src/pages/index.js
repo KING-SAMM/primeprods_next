07c178912ddef49fd5ee487e87f6494bb033808d
kcsamm <kcsamm11@gmail.com>
2022-09-14 09:08:40 +0100

    Implemented previous and next page functionalities

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Card from '@/components/Card'
 import { getAllPrototypes } from "@/lib/fetch";
 import { useAuth } from '@/hooks/auth'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PrototypeCard from '@/components/PrototypeCard'
 import GuestNavigationDark from '@/components/Layouts/GuestNavigationDark';
 
@@ -12,13 +12,16 @@ export default function Home({ ...prototypesObj }) {
 
     const [dark, setDark] = useState(true);
 
-    const {
+    let {
+        first_page,
+        last_page,
         first_page_url,
         last_page_url,
+        prev_page_url,
+        next_page_url,
         current_page,
         data,
         from,
-        last_page,
     } = prototypesObj;
 
     // console.log(current_page, data, from, last_page, last_page_url, first_page_url);
@@ -29,24 +32,64 @@ export default function Home({ ...prototypesObj }) {
     const imgUrl = "https://www.notebookcheck.net/fileadmin/_processed_/f/3/csm_csm_Oppo_Watch_3_Render_2_7ef6882bff_4393f5078f.jpg"
 
     const [prototypesList, setPrototypesList] = useState(data);
-    const [page, setPage] = useState(1);
+    let [currentPage, setCurrentPage] = useState(1);
+
+    console.log("type of current_page: ", typeof(current_page));
+
+    console.log("Initial current_page is: ", current_page);
 
     // console.log("Prototypes list is: ", prototypesList)
 
-    const loadPreviousPrototypes = async (res) => {
-        res =  await fetch(`http://localhost:8000/api?page=${ page - 1 }`);
-        res = await res.json();
-        res = res.prototypes;
+    const loadPreviousPrototypes = async (prevRes) => {
+        current_page = currentPage;
+        
+        (current_page <= first_page) ? setCurrentPage(first_page): (current_page = current_page - 1);
+        
+        prevRes =  await fetch(`http://localhost:8000/api?page=${ current_page }`);
+        prevRes = await prevRes.json();
+        prevRes = prevRes.prototypes;
 
-        ({ current_page, data } = res );
+        ({ current_page, data } = prevRes );
 
-        console.log("New page is: ", current_page, data);
+        currentPage = current_page;
 
-        setPrototypesList(res => [...res]);
+        console.log("Current page match? " + currentPage + " " + current_page)  // same
+
+        console.log("New page is: ", current_page, data); // 2
+
+        setPrototypesList(prevRes => [...prevRes]);
+
+        setCurrentPage(current_page);
     }
 
-    const loadNextPrototypes = async () => {};
+    
+    const loadNextPrototypes = async (nextRes) => {
+        current_page = currentPage;
+        
+        (current_page >= last_page) ? setCurrentPage(last_page): (current_page = current_page + 1);
+        
+        nextRes =  await fetch(`http://localhost:8000/api?page=${ current_page }`);
+        nextRes = await nextRes.json();
+        nextRes = nextRes.prototypes;
 
+        ({ current_page, data } = nextRes );
+
+        currentPage = current_page;
+
+        console.log("Current page match? " + currentPage + " " + current_page)  // 1, 2
+
+        console.log("New page is: ", current_page, data); // 2
+
+        setPrototypesList(nextRes => [...nextRes]);
+
+        setCurrentPage(current_page);
+    };
+    
+
+    useEffect(() => {
+        setCurrentPage(current_page)
+    }, [current_page]);
+        
     return (
         <>
             <Head>
@@ -58,7 +101,7 @@ export default function Home({ ...prototypesObj }) {
                     {/* Logo */}
                     {/* <div className="flex-shrink-0">
                         <Link href="/">
-                            <a> 
+                            <a> \\
                                 <img
                                     src='/images/logo4.PNG'
                                     className='block h-12 w-auto fill-current'
